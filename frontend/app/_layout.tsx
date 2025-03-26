9bf7b0d386db9e17eb4cf599fd87aab5ab345003
unknown <dev.code38@gmail.com>
2025-03-26 18:40:51 +0530

    initial commit & added clerk

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,13 +1,17 @@
-import { Slot } from 'expo-router';
-import { PaperProvider } from 'react-native-paper';
-import { AuthProvider } from '../context/AuthContext';
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { Slot } from 'expo-router'
 
 export default function RootLayout() {
+    const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+    if (!publishableKey) {
+        throw new Error('Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env')
+    }
+
     return (
-        <AuthProvider>
-            <PaperProvider>
-                <Slot />
-            </PaperProvider>
-        </AuthProvider>
-    );
+        <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+            <Slot />
+        </ClerkProvider>
+    )
 }
